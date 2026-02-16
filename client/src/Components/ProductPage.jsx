d4d616c12b300c30768d92457d61dfa805f2feaf
sainaresh2727 <sainaresh2727@gmail.com>
2026-02-16 13:42:08 +0530

    correct margin issues in product dynamic page

--- client/src/Components/ProductPage.jsx
+++ client/src/Components/ProductPage.jsx
@@ -21,10 +21,10 @@ function ProductPage() {
     <section className='container-fluid' id='dynamicpagecf'>
     <div className="container">
     <div className="row p-3" id='rowdynamic'>
-    <div className="col-lg-6" id='pdctpageimgparent'>
+    <div className="col-lg-6 col-12" id='pdctpageimgparent'>
     <img src={Findpdct.ProductImg} alt="" />
     </div>
-    <div className="col-lg-6 mt-5" id='dynamicpagecontent'>
+    <div className="col-lg-6 mt-5 col-12" id='dynamicpagecontent'>
     <h1 className='text-center' id='smallscheading'>{Findpdct.ProductName}</h1>
     <p>{ Findpdct.ProductDes}</p>
     <div className='' id='pricecategory'>
